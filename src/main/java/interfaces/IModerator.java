8087191c60b10b5fe3bd67cd2e579c9c8d5334ea
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-11-11 21:48:56 +0100

    some changes in the logic

---
+++ src/main/java/interfaces/IModerator.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+
+public interface IModerator {
+
+    void checkModerator ();
+
+}
--- src/main/java/model/Moderator.java
+++ src/main/java/model/Moderator.java
@@ -1,4 +1,32 @@
 package model;
 
-public class Moderator {
+import interfaces.IModerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Moderator  implements IModerator {
+
+
+    private final List<String> moderatorList = new ArrayList<>();
+
+    @Override
+    public void checkModerator() {
+
+        //1. 
+
+
+        //2.
+
+
+        //3.
+
+
+        //4.
+
+
+
+
+
+    }
 }
