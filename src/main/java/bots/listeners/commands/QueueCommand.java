534a8780378db44329b41bff91ddbd1de9aeb806
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2021-11-23 23:59:18 +0100

    Added new lines to setup the logic for queuecommand

--- src/main/java/bots/listeners/commands/QueueCommand.java
+++ src/main/java/bots/listeners/commands/QueueCommand.java
@@ -1,13 +1,29 @@
 package bots.listeners.commands;
 
+import bots.helpers.AudioManager;
+import bots.helpers.LavaPlayerAudioSource;
+import bots.helpers.PlayerManager;
+import bots.helpers.ServerMusicManager;
+import bots.listeners.SaitamaAudioListener;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import models.ServerCommand;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.javacord.api.audio.AudioSource;
 import org.javacord.api.entity.channel.ServerTextChannel;
 import org.javacord.api.entity.server.Server;
 import org.javacord.api.entity.user.User;
 import org.javacord.api.event.message.MessageCreateEvent;
+import service.MessageBuilderService;
 
 public class QueueCommand extends ServerCommand {
 
+
+    private MessageBuilderService messageBuilderService = new MessageBuilderService();
+
+    private final AudioPlayerManager playerManager = PlayerManager.getManager();
+
+    private static final Logger logger = LogManager.getLogger(SaitamaAudioListener.class);
 
     public QueueCommand(){
         super("!queue");
@@ -16,6 +32,20 @@ public class QueueCommand extends ServerCommand {
     @Override
     protected void runCommand(MessageCreateEvent event, Server server, ServerTextChannel channel, User user, String[] args) {
 
+        logger.info("The ");
+        System.out.println(" ");
+        ServerMusicManager m = AudioManager.get(event.getServer().get().getId());
+        AudioSource source = new LavaPlayerAudioSource(event.getApi(), m.player);
+
+        //1. Set the AudioSource
+
+        //2. Set the TrackScheduler
+
+        //3. Get the list in order with
+
         
+        //4. Set it pretty
+
+
     }
 }
